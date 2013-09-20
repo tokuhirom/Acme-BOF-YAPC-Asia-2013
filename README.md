8c7003a99959abf126d00d0edeb489883580a048
tokuhirom <tokuhirom@gmail.com>
2013-09-20 14:25:04 +0900

    Checking in changes prior to tagging of version 0.01.
    Changelog diff is:
    
    diff --git a/Changes b/Changes
    index d754f74..fb39a73 100644
    --- a/Changes
    +++ b/Changes
    @@ -2,5 +2,7 @@ Revision history for Perl extension Acme-BOF-YAPC-Asia-2013
    
     {{$NEXT}}
    
    +0.01 2013-09-20T05:24:57Z
    +
         - original version

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+Acme::BOF::YAPC::Asia::2013 - It's new $module
+
+# SYNOPSIS
+
+    use Acme::BOF::YAPC::Asia::2013;
+
+# DESCRIPTION
+
+Acme::BOF::YAPC::Asia::2013 is ...
+
+# LICENSE
+
+Copyright (C) tokuhirom.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+tokuhirom <tokuhirom@gmail.com>
---
+++ lib/Acme/BOF/YAPC/Asia/2013.pm
@@ -0,0 +1,39 @@
+package Acme::BOF::YAPC::Asia::2013;
+use 5.008005;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+sub udzulla { 'bento' x 4 }
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Acme::BOF::YAPC::Asia::2013 - It's new $module
+
+=head1 SYNOPSIS
+
+    use Acme::BOF::YAPC::Asia::2013;
+
+=head1 DESCRIPTION
+
+Acme::BOF::YAPC::Asia::2013 is ...
+
+=head1 LICENSE
+
+Copyright (C) tokuhirom.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+tokuhirom E<lt>tokuhirom@gmail.comE<gt>
+
+=cut
+
